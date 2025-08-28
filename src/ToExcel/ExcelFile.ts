2f0329496c7ee2f2511b6a3cad6648d1a095f3c0
2279679717@qq.com <cy@cyminideMac-mini.local>
2025-08-28 14:14:54 +0800

    使用Exceljs

--- src/ToExcel/ExcelFile.ts
+++ src/ToExcel/ExcelFile.ts
@@ -43,3 +43,25 @@ export async function exportSheetExcelCy(name: any, customData: any) {
   const fileData = new Blob([buffer]);
   fileSaver.saveAs(fileData, `${name}.xlsx`);
 }
+
+export async function exportSheetExcelByWorkbook(
+  name: any,
+  customData: any,
+  workbook: any
+) {
+  // const workbook = new ExcelJS.Workbook();
+  customData.every(function (table: any) {
+    if (table?.data?.length === 0) return true;
+    const worksheet = workbook.addWorksheet(table.name);
+    setStyleAndValue(table, worksheet);
+    setMerge(table?.config?.merge, worksheet);
+    setBorder(table, worksheet);
+    setImages(table, worksheet, workbook);
+    setDataValidations(table, worksheet);
+    setHiddenRowCol(table, worksheet);
+    return true;
+  });
+  const buffer = await workbook.xlsx.writeBuffer();
+  const fileData = new Blob([buffer]);
+  fileSaver.saveAs(fileData, `${name}.xlsx`);
+}
--- src/common/Transform.ts
+++ src/common/Transform.ts
@@ -1,35 +1,42 @@
-
 import { FortuneFile } from "../ToFortuneSheet/FortuneFile";
 import { HandleZip } from "../ToFortuneSheet/HandleZip";
 import { exportSheetExcel } from "../ToExcel/ExcelFile";
 
 export const transformExcelToFortune = async (
-    e: any,
-    setSheets: any,
-    setKey: any,
-    sheetRef: any
+  e: any,
+  setSheets: any,
+  setKey: any,
+  sheetRef: any
 ) => {
-    const excelFile = await e.target.files[0].arrayBuffer();
-    const files = await new HandleZip(excelFile).unzipFile();
-    const fortuneFile = new FortuneFile(files, excelFile.name);
-    fortuneFile.Parse();
+  const excelFile = await e.target.files[0].arrayBuffer();
+  const files = await new HandleZip(excelFile).unzipFile();
+  const fortuneFile = new FortuneFile(files, excelFile.name);
+  fortuneFile.Parse();
 
-    const lsh = fortuneFile.serialize();    
+  const lsh = fortuneFile.serialize();
 
-    setSheets(lsh.sheets);
-    setKey((k: number) => k + 1);
+  setSheets(lsh.sheets);
+  setKey((k: number) => k + 1);
 
-    setTimeout(() => {
-        for (let sheet of lsh.sheets) {
-            let config = sheet.config;
-            sheetRef.current?.setColumnWidth(config?.columnlen || {}, { id: sheet.id });
-            sheetRef.current?.setRowHeight(config?.rowlen || {}, { id: sheet.id });
-        }
-    }, 1);
+  setTimeout(() => {
+    for (let sheet of lsh.sheets) {
+      let config = sheet.config;
+      sheetRef.current?.setColumnWidth(config?.columnlen || {}, {
+        id: sheet.id,
+      });
+      sheetRef.current?.setRowHeight(config?.rowlen || {}, { id: sheet.id });
+    }
+  }, 1);
 };
 
-export const transformFortuneToExcel = async (
-    luckysheetRef: any
-) => {
-    await exportSheetExcel(luckysheetRef);
+export const transformExcelToFortuneCy = async (excelFile: any) => {
+  const files = await new HandleZip(excelFile).unzipFile();
+  const fortuneFile = new FortuneFile(files, excelFile.name);
+  fortuneFile.Parse();
+  const lsh = fortuneFile.serialize();
+  return lsh;
+};
+
+export const transformFortuneToExcel = async (luckysheetRef: any) => {
+  await exportSheetExcel(luckysheetRef);
 };
